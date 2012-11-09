e3b49b63bdfa811da17f01de41e48627e6669520
whitypig <whitypig@gmail.com>
2012-11-10 06:03:00 +0900

    Added testTagClass() test method.

--- TagsTest.java
+++ TagsTest.java
@@ -1,3 +1,24 @@
+import static org.junit.Assert.*;
+
+import java.util.Vector;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
 public class TagsTest {
-  
+
+  @Test
+  public void testTagClass() {
+    try {
+      Method target = Tags.class.getDeclaredMethod("tagClass", java.lang.Class.class);
+      target.setAccessible(true);
+      //ClassItem clsItem = (ClassItem)target.invoke(new Tags(), org.junit.Test.class);
+      ClassItem clsItem = (ClassItem)target.invoke(new Tags(), java.util.Vector.class);
+      assertEquals("Vector", clsItem.name);
+      assertEquals("java.util", clsItem.pkgItem.name);
+    } catch (Exception e) {
+      assertTrue(e.getCause() instanceof ApplicationException);
+    }
+  }
 }
