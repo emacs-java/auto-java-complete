e6215477a9745a4b06a21f31815e8840ae413234
whitypig <whitypig@gmail.com>
2012-12-24 14:52:41 +0900

    Added TestTags.java.

---
+++ src/test/java/TestTags.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTags {
+  @Test
+  public void testTagClass() {
+    fail("implement me!");
+  }
+}
