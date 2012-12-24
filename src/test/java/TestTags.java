848232cab46122cb0e56d8425b4aa425584cc238
whitypig <whitypig@gmail.com>
2012-12-24 17:09:32 +0900

    Implemented testTagClassOk().

--- src/test/java/TestTags.java
+++ src/test/java/TestTags.java
@@ -1,10 +1,17 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestTags {
   @Test
-  public void testTagClass() {
-    fail("implement me!");
+  public void testTagClassOk() throws ApplicationException {
+    Tags tags = new Tags();
+    ClassItem cItem = tags.tagClass(java.util.Vector.class);
+    assertEquals(java.util.Vector.class, cItem.getClazz());
+    assertEquals("Vector", cItem.getName());
+    assertEquals("package name should be java.util", "java.util", cItem.getPackageName());
+    assertTrue(tags.getPackages().contains(cItem.getPackageItem()));
   }
 }
