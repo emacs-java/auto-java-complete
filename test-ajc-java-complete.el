9a464e6bc324872b4b8b1a7afa673c356f228163
whitypig <whitypig@gmail.com>
2013-01-19 06:33:19 +0900

    Added some tests.

--- test-ajc-java-complete.el
+++ test-ajc-java-complete.el
@@ -90,3 +90,45 @@
                  '("System" "." "getProperty" "." "t"))
                 (ajc-complete-method-candidates-cache nil))
             (ajc-complete-method-is-available "System.getProperty(str.substring(3)).to")))))
+
+(ert-deftest test-ajc-calculate-all-imported-class-items ()
+  (should
+   (equal '("File" "Test" "Vector")
+          (sort (mapcar #'car (with-temp-buffer
+                                (insert "import java.io.File;")
+                                (newline)
+                                (insert "import java.util.Vector;")
+                                (newline)
+                                (insert "import org.junit.Test;")
+                                (insert "class ClassName {")
+                                (newline)
+                                (ajc-calculate-all-imported-class-items t)))
+                #'string<)))
+  ;; todo add testcase for import java.util.*; statement.
+  )
+
+(ert-deftest test-ajc-find-out-matched-class-item-without-package-prefix ()
+  (should
+   (equal '("StringBufferInputStream" "StringBuffer" "StringBuilder")
+          (mapcar #'car
+                  (ajc-find-out-matched-class-item-without-package-prefix "StringB"))))
+  (should
+   (equal '"Vector"
+          (caar (ajc-find-out-matched-class-item-without-package-prefix "Vector")))))
+
+(ert-deftest test-ajc-fqn-candidates-1 ()
+  (should
+   (equal "PI"
+          (caar (ajc-fqn-candidates-1 "java.lang.Math.P"))))
+  (should
+   (null (ajc-fqn-candidates-1 "foo.bar.b"))))
+
+(ert-deftest test-ajc-package-candidates ()
+  (should
+   (equal '("java.util.Vector")
+          (ajc-package-candidates "java.util.V")))
+  (should
+   (equal '("java.lang.Math")
+          (ajc-package-candidates "java.lang.M")))
+  (should
+   (null (ajc-package-candidates "foo.bar."))))
