7128f0168231cc41e534fc9d2720821f7d62a063
whitypig <whitypig@gmail.com>
2013-01-26 04:06:36 +0900

    Fixed ugly messages.

--- src/main/java/TagsMain.java
+++ src/main/java/TagsMain.java
@@ -34,48 +34,54 @@ public class TagsMain {
 
   private void printStartMessage() {
     System.out.println(
-      "*****************************************************************\n" +
-      "**   this program will need about 3 to 15 min,                  **\n" +
-      "**   maybe half an hour ,(just kidding),but you must be patient.**\n" +
-      "**   before it exit,you may see a few exceptions                **\n" +
-      "**   If it don't kill the program ,just ignore it .             **\n" +
-      "**   and when you add a jar to classpath, you'd better make sure**\n" +
-      "**   all jars it depends on are in classpath.                   **\n" +
+      "******************************************************************\n" +
+      "***   This program will need about 3 to 15 min,                ***\n" +
+      "***   so you you have to be patient                            ***\n" +
+      "***   Before exiting, you may see a few exceptions.            ***\n" +
+      "***   If it don't kill the program, just ignore it.            ***\n" +
+      "***   When you add a jar to classpath, you'd better make sure  ***\n" +
+      "***   all jars it depends on are in classpath.                 ***\n" +
       "******************************************************************\n"
       );
 
-    System.out.println("log file is located at: " +
+    System.out.println("Log file is located at: " +
                        System.getProperty("java.io.tmpdir") + "/ajc_error.log");
-    System.out.println("log file is located at: " +
+    System.out.println("Log file is located at: " +
                        System.getProperty("java.io.tmpdir") +
                        "/ajc_info.log\n\n");
 
     System.out.println(
-      "********************************************************************************************\n" +
-      "***    you can use this Class like this:                                                 ***\n" +
-      "***           java Tags                                                                  ***\n" +
-      "***    all class  in classpath will be tagged.                                           ***\n" +
-      "***                                                                                      ***\n" +
-      "***           java Tags \"org\\.hello,org\\.world\"                                      ***\n" +
-      "***    it would NOT tag those class match \"org.hello\" or \"org.world\" .               ***\n" +
-      "***                                                                                      ***\n" +
-      "***           java -cp yourclasspath Tags                                                ***\n" +
-      "***  if you see java.lang.OutOfMemoryError: PermGen space ,you can increment permsize:   ***\n" +
-      "***       java -XX:MaxPermSize=512m -Xms256M -Xmx512M Tags                               ***\n" +
-      "***                                                                                      ***\n" +
-      "***  before that you'd better backup the  file ~/.java_base.tag,if exists                ***\n" +
-      "*******************************************************************************************\n\n"
+      "******************************************************************\n" +
+      "***    You can use this Class like this:                       ***\n" +
+      "***           java Tags                                        ***\n" +
+      "***    all classes in classpath will be tagged.                ***\n" +
+      "***                                                            ***\n" +
+      "***           java Tags \"org\\.hello,org\\.world\"            ***\n" +
+      "***    This would NOT tag those classes that match             ***\n" +
+      "***    \"org.hello\" or \"org.world\".                         ***\n" +
+      "***                                                            ***\n" +
+      "***           java -cp yourclasspath Tags                      ***\n" +
+      "***  If you see java.lang.OutOfMemoryError: PermGen space,     ***\n" +
+      "***  you need to increase permsize:                            ***\n" +
+      "***       java -XX:MaxPermSize=512m -Xms256M -Xmx512M Tags     ***\n" +
+      "***                                                            ***\n" +
+      "***  Before that, you'd better backup the file                 ***\n" +
+      "***   ~/.java_base.tag if exists                               ***\n" +
+      "******************************************************************\n\n"
       );
   }
 
   private void printEndMessage() {
     System.out.println(
-      "\n******************************************************************************************\n" +
-      "***                  exit successful!!!                                                  ***\n" +
-      "***you will see a file named '.java_base.tag' in your home directory                     ***\n" +
-      "***  the size of the generated ~/.java_base.tag  is about 2M or bigger,so if your        ***\n" +
-      "*** .java_base.tag  is too small ,that means your CLASSPATH don't configure correctly.   ***\n" +
-      "********************************************************************************************\n"
+      "\n****************************************************************\n" +
+      "***                  Exit successful!!!                        ***\n" +
+      "***  You will see a file named '.java_base.tag'                ***\n" +
+      "***  in your home directory.                                   ***\n" +
+      "***  The size of the generated ~/.java_base.tag is             ***\n" +
+      "***  about 2M or bigger, so if your .java_base.tag is          ***\n" +
+      "***  too small, that mabey means your CLASSPATH isn't          ***\n" +
+      "***  configureed properly.                                     ***\n" +
+      "******************************************************************\n"
       );
     System.out.println(new File(_tags.getHomePath(), ".java_base.tag").getAbsolutePath());
   }
